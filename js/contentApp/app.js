0180b234e83a4873e9923388345c9b16bcf8ec93
mrugeles <mrugeles@gmail.com>
2016-05-21 19:12:16 -0500

    adding routes for contents

--- js/contentApp/app.js
+++ js/contentApp/app.js
@@ -3,5 +3,39 @@
 var contentApp = angular.module('contentApp', [
     'ngSanitize',
     'btford.markdown',
+    'ngRoute',
     'contentControllers'
 ]);
+
+contentApp.run(['$rootScope', function($rootScope){
+    $rootScope.content = null;
+}]);
+
+contentApp.config(['$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(false);
+        $routeProvider.
+        when('/', {
+          templateUrl: 'views/home.html',
+          controller: 'HomeCtrl'
+        }).
+        when('/acda', {
+          templateUrl: 'views/acda.html',
+          controller: 'AcdaCtrl'
+        }).
+        when('/portfolio', {
+          templateUrl: 'views/portfolio.html',
+          controller: 'PortfolioCtrl'
+        }).
+        when('/bass', {
+          templateUrl: 'views/bass.html',
+          controller: 'BassCtrl'
+        }).
+        when('/contact', {
+          templateUrl: 'views/contact.html',
+          controller: 'ContactCtrl'
+        }).
+        otherwise({
+          redirectTo: '/'
+        });
+  } ]);
--- js/contentApp/controllers.js
+++ js/contentApp/controllers.js
@@ -4,17 +4,41 @@
 
 var contentControllers = angular.module('contentControllers', []);
 
-contentControllers.controller('PageCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-
+contentControllers.controller('NavCtrl', ['$scope', '$http', '$location', '$rootScope',
+  function ($scope, $http, $location, $rootScope) {
+        $scope.currentContent = $location.url().replace('/', '');
         $scope.init = function(){
             $scope.setLang('en');
         }
         $scope.setLang = function(lang){
-            $http.get('lang/'+lang+'.json').success(function(data) {
-                $scope.lang = data;
+            $http.get('lang/'+lang+'/menu.json').success(function(data) {
+                $scope.menu = data;
+            });
+            $http.get('lang/'+lang+'/'+$scope.currentContent+'.json').success(function(data) {
+                $rootScope.content = data;
             });
         }
 
         $scope.init();
   } ]);
+
+  contentControllers.controller('HomeCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+
+    } ]);
+  contentControllers.controller('AcdaCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+
+    } ]);
+  contentControllers.controller('PortfolioCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+
+    } ]);
+  contentControllers.controller('BassCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+
+    } ]);
+  contentControllers.controller('ContactCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+
+    } ]);
