611a6cc21e305a356bed509306c4e10a61b49bda
mrugeles <mrugeles@gmail.com>
2016-05-29 18:22:39 -0500

    fixing change lang

--- js/contentApp/controllers.js
+++ js/contentApp/controllers.js
@@ -11,21 +11,36 @@ contentControllers.controller('NavCtrl', ['$scope', '$http', '$location', '$root
         }
         $scope.setLang = function(lang){
             $scope.currentContent = $location.url().replace('/', '');
-            $rootScope.lang = lang;
             $http.get('lang/'+lang+'/menu.json').success(function(data) {
                 $scope.menu = data;
             });
+
+            $rootScope.$emit("CallSetLang", {currentLang:lang});
         }
 
         $scope.init();
   } ]);
 
-  contentControllers.controller('CMSCtrl', ['$scope', '$http', '$routeParams', '$sce',
-    function ($scope, $http, $routeParams,$sce) {
-        $http.get('lang/'+$routeParams.lang+'/'+$routeParams.page+'.json').success(function(data) {
-            $scope.content = data;
-            $scope.view = 'views/'+$routeParams.page+'.html';
+  contentControllers.controller('CMSCtrl', ['$scope', '$http', '$routeParams', '$rootScope', '$sce',
+    function ($scope, $http, $routeParams, $rootScope, $sce) {
+
+        $scope.init = function(lang){
+            $scope.setLang(lang);
+        }
+
+        $scope.setLang = function(lang){
+            $rootScope.lang = lang;
+            $http.get('lang/'+lang+'/'+$routeParams.page+'.json').success(function(data) {
+                $scope.content = data;
+                $scope.view = 'views/'+$routeParams.page+'.html';
+            });
+        }
+
+        $rootScope.$on("CallSetLang", function(event, args){
+             $scope.setLang(args.currentLang);
         });
+
+        $scope.init($routeParams.lang);
     } ]);
 
 
